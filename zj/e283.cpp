5ec782f5becff2278940b6899a45d16f84726b4b
白目中中 <yijohnjohn@gmail.com>
2020-07-20 16:08:34 +0800

    cin:int
    還是不行..

--- zj/e283.cpp
+++ zj/e283.cpp
@@ -1,31 +1,29 @@
 #include<iostream>
-#include<string>
 using namespace std;
 
 int main(){
 	ios::sync_with_stdio(false);
 	cin.tie(0);
-	int n;
-	string x,s1="0 1 0 1",s2="0 1 1 1",s3="0 0 1 0",s4="1 1 0 1",s5="1 0 0 0",s6="1 1 0 0";
+	int n,x=0,y=1;
+	//A="0 1 0 1",B="0 1 1 1",C="0 0 1 0",D="1 1 0 1",E="1 0 0 0",F="1 1 0 0"
 	while (cin>>n){
 		while (n--){
-			cin>>x;
-			if (x==s1){
+			if (cin>>x>>y>>x>>y){
 				cout<<'A';
 			}
-			if (x==s2){
+			if (cin>>x>>y>>y>>y){
 				cout<<'B';
 			}
-			if (x==s3){
+			if (cin>>x>>x>>y>>x){
 				cout<<'C';
 			}
-			if (x==s4){
+			if (cin>>y>>y>>x>>y){
 				cout<<'D';
 			}
-			if (x==s5){
+			if (cin>>y>>x>>x>>x){
 				cout<<'E';
 			}
-			if (x==s6){
+			if (cin>>y>>y>>x>>x){
 				cout<<'F';
 			}
 		}

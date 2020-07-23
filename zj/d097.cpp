3c29a1f87ba603a433b5911300118cd5c85bcfb4
白目中中 <yijohnjohn@gmail.com>
2020-07-23 13:32:53 +0800

    .

---
+++ zj/d097.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main(){
+	int n;
+	while (cin>>n){
+		int a[3000],d[3000]={0},x=0,ans[3000]=0;
+		for (int i=0;i<n;i++){
+			cin>>a[i];
+			x=abs(a[i+1]-a[i]);
+			d[x]=1;
+		}
+		for (int i=1;i<=n-1;i++){
+			if (d[i]==0){
+				ans[i]=1;
+				break;
+			}
+		}
+		if (ans[i]==1){
+			cout<<"Not jolly"<<endl;
+		}
+		else {
+			cout<<"Jolly"<<endl;
+		}
+	}
+	return 0;
+}

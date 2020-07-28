da2be530ce4847197f5df0fe3f9286d4b8cba6d3
白目中中 <yijohnjohn@gmail.com>
2020-07-28 21:31:53 +0800

    .

--- zj/a038.cpp
+++ zj/a038.cpp
@@ -4,6 +4,16 @@ using namespace std;
 int main(){
 	int n;
 	cin>>n;
-	int a[10000000000];
+	while (n%10==0){
+		n/=10;
+		if (n==0){
+			cout<<'0';
+			break;
+		}
+	}
+	while (n>0){
+		cout<<n%10;
+		n/=10;
+	}
 	return 0;
 }

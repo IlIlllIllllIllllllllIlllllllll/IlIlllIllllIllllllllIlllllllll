3d3258351d03650d21d0948c6521efbddeb4255a
白目中中 <yijohnjohn@gmail.com>
2020-08-12 22:30:50 +0800

    小改

--- zj/d018_c++11.cpp
+++ zj/d018_c++11.cpp
@@ -9,17 +9,33 @@ int main(){
 	while (getline(cin,input)){
 		stringstream ss(input);
 		string s;
-		string::size_type sz;
 		double sum=0,x,dif=0,y;
 		while (ss>>s){
-			if (s[0]%2==1){
-				s.erase(s.begin(),s.begin()+2);
-				x=stod(s,&sz);
+			int check;
+			for (int i=0;i<s.length();i++){
+				if (s[i]==':'){
+					check=(int)s[i-1];
+					break;
+				}
+			}
+			if (check%2==1){
+				for (int i=0;i<s.length();i++){
+					if (s[i]==':'){
+						s.erase(s.begin(),s.begin()+i+1);
+						break;
+					}
+				}
+				x=stod(s);
 				sum+=x;
 			}
 			else {
-				s.erase(s.begin(),s.begin()+2);
-				y=stod(s,&sz);
+				for (int i=0;i<s.length();i++){
+					if (s[i]==':'){
+						s.erase(s.begin(),s.begin()+i+1);
+						break;
+					}
+				}
+				y=stod(s);
 				dif+=y;
 			}
 		}

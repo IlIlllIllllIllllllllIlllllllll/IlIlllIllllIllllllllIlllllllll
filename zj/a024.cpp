0daa3e0748226c9ef16b723b17c79f854fef9461
白目中中 <yijohnjohn@gmail.com>
2020-10-14 18:22:40 +0800

    Create a024.cpp

---
+++ zj/a024.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int gcd(int a,int b){
+	if (b>0){
+		return gcd(b,a%b);
+	}
+	else return a;
+}
+
+int main(){
+	cin.tie(0);
+	cin.sync_with_stdio(0);
+	int a,b;
+	cin>>a>>b;
+	cout<<gcd(a,b)<<'\n';
+	return 0;
+}
